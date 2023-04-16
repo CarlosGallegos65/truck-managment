aa9703563edad0d006a4adb8ca0ea5723ff9cbca
AbadonCoder <122395282+AbadonCoder@users.noreply.github.com>
2023-04-16 11:26:41 -0600

    Update formatDates.js

--- helpers/formatDates.js
+++ helpers/formatDates.js
@@ -1,6 +1,6 @@
 
 const formatDate = (date) => {  
-    const newDate = date.toLocaleString("es-MX", date.getTimezoneOffset());
+    const newDate = date.toLocaleString("es-MX", {timeZone: "America/Mexico_City"});
     return newDate;
 }
 
